a56b124330299435135d17291a4ad57619ce3e7b
ddd <ddd@DESKTOP-E2L0SUU>
2019-11-26 10:42:42 +0900

    Git_Second

---
+++ 2019_11_25/src/Ex1.java
@@ -0,0 +1,13 @@
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int a = 10;
+		byte b = 127;
+		double c = 100.7;
+		double d = a*0.1;
+		d = (int)(b - 100);
+		System.out.println(Float.SIZE);
+		System.out.println(d);
+		System.out.println(c);
+	}
+}
