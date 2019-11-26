eda229fd9469d57ab6c19f2258cae8d9e5fd38da
ddd <ddd@DESKTOP-E2L0SUU>
2019-11-26 09:46:54 +0900

    First Commit

---
+++ 2019_11_25/src/Ex8.java
@@ -0,0 +1,16 @@
+/*
+*이프로그램은 주석을 위한 프로그램입니다.
+*작성일 : 2019년 11월 25일
+*작성자 : 염광래
+*import java.lang.*;
+*/
+
+
+
+public class Ex8 {//파일제목과 클래스제목은 동일.
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println("ws" + x++);
+		System.out.println("ws" + x);
+	}
+}
